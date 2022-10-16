d2c4778ada4f16194ee611ae90ec81cf18089d8a
melloyk <kmelloy98@gmail.com>
2022-10-16 15:57:55 -0400

    fix tab

--- john.py
+++ john.py
@@ -19,7 +19,7 @@ async def on_ready():
 		print(f'{guild.name}(id: {guild.id})')
 
 		members = '\n - '.join([member.name for member in guild.members])
-    	print(f'Guild Members:\n - {members}')
+		print(f'Guild Members:\n - {members}')
 
 	print(f'{client.user} has connected to Discord!')
 
