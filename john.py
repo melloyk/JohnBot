b2a1725c91f58c495283b2d3d5fb70de40a596b3
melloyk <kmelloy98@gmail.com>
2022-10-16 15:55:03 -0400

    add members

--- john.py
+++ john.py
@@ -14,9 +14,12 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-	for guild in client.guild:
+	for guild in client.guilds:
 		print(f'{client.user} is connected to the following guild:\n')
 		print(f'{guild.name}(id: {guild.id})')
+
+		members = '\n - '.join([member.name for member in guild.members])
+    	print(f'Guild Members:\n - {members}')
 
 	print(f'{client.user} has connected to Discord!')
 
