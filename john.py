853c101fa4cf327b1e9538bdbc7f5f3b1d016e30
melloyk <kmelloy98@gmail.com>
2022-10-16 19:35:25 +0000

    add intents

--- john.py
+++ john.py
@@ -6,10 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('JOHN_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.all()
+intents.members = False
+intents.presences = False
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    for guild in client.guild:
+        print(f'{client.user} is connected to the following guild:\n')
+        print(f'{guild.name}(id: {guild.id})')
+
 	print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
